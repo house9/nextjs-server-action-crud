9e28528abd586c1e69220f7ed10a73b6ee384014
Jesse House <jesse.house@codingzeal.com>
2023-09-29 10:25:41 -0700

    add content to public home page

--- src/app/(application-layout)/components/Sidebar.tsx
+++ src/app/(application-layout)/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export const Sidebar = () => {
   return (
     <>
@@ -84,7 +86,10 @@ export const Sidebar = () => {
         >
           <ul className="space-y-1.5">
             <li>
-              <a className="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-900 dark:text-white">
+              <Link
+                href="/dashboard"
+                className="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-900 dark:text-white"
+              >
                 <svg
                   className="w-3.5 h-3.5"
                   xmlns="http://www.w3.org/2000/svg"
@@ -103,7 +108,7 @@ export const Sidebar = () => {
                   />
                 </svg>
                 Dashboard
-              </a>
+              </Link>
             </li>
 
             <li className="hs-accordion" id="users-accordion">
@@ -439,7 +444,10 @@ export const Sidebar = () => {
               </a>
             </li>
             <li>
-              <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300">
+              <Link
+                href="/"
+                className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300"
+              >
                 <svg
                   className="w-3.5 h-3.5"
                   xmlns="http://www.w3.org/2000/svg"
@@ -450,8 +458,8 @@ export const Sidebar = () => {
                 >
                   <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
                 </svg>
-                Documentation
-              </a>
+                Home
+              </Link>
             </li>
           </ul>
         </nav>
--- src/app/(public-layout)/home/page.tsx
+++ src/app/(public-layout)/home/page.tsx
@@ -1,11 +1,73 @@
+import Link from "next/link";
+
 export default function Home() {
   console.log("  render: Home: app/(public-layout)/home/page.tsx");
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>
-        <a href="/dashboard">Dashboard</a>
-      </p>
+      {/* <!-- Testimonials --> */}
+      <div className="relative overflow-hidden">
+        <div className="max-w-[85rem] px-4 py-12 sm:px-6 lg:px-8 lg:py-16 mx-auto">
+          {/* <!-- Gradients --> */}
+          <div aria-hidden="true" className="flex absolute left-0 -z-[1]">
+            <div className="bg-purple-200 opacity-20 blur-3xl w-[1036px] h-[300px] dark:bg-purple-900 dark:opacity-20"></div>
+          </div>
+          {/* <!-- End Gradients --> */}
+
+          {/* <!-- Grid --> */}
+          <div className="lg:grid lg:grid-cols-6 lg:gap-8 lg:items-center">
+            <div className="hidden lg:block lg:col-span-2">
+              <img
+                className="rounded-xl"
+                src="scott-webb-1ddol8rgUH8-unsplash-small.jpg"
+                alt="Image Description"
+              />
+            </div>
+            {/* <!-- End Col --> */}
+
+            <div className="lg:col-span-4">
+              {/* <!-- Blockquote --> */}
+              <blockquote>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src="sunder-muthukumaran-fd6K_OFlnRA-unsplash-small.jpg"
+                  alt="Image Description"
+                />
+
+                <p className="text-xl font-medium text-gray-800 lg:text-2xl lg:leading-normal dark:text-gray-200">
+                  Aut similique magnam quia suscipit non quisquam eos veritatis.
+                  Et est quas placeat est enim magni voluptas ducimus sapiente
+                  qui facere quod. Eos harum et beatae rerum aut aut tempore
+                  asperiores impedit.
+                </p>
+
+                <footer className="mt-6">
+                  <div className="flex items-center">
+                    <div className="lg:hidden flex-shrink-0">
+                      <img
+                        className="h-12 w-12 rounded-full"
+                        src="scott-webb-1ddol8rgUH8-unsplash-small.jpg"
+                        alt="Image Description"
+                      />
+                    </div>
+                    <div className="ml-4 lg:ml-0">
+                      <Link
+                        href="/dashboard"
+                        className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-green-500 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+                      >
+                        Enter
+                      </Link>
+                    </div>
+                  </div>
+                </footer>
+              </blockquote>
+              {/* <!-- End Blockquote --> */}
+            </div>
+            {/* <!-- End Col --> */}
+          </div>
+          {/* <!-- End Grid --> */}
+        </div>
+      </div>
+      {/* <!-- End Testimonials --> */}
     </div>
   );
 }
