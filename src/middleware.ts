73228eafae21d46c7443b57b588c15fac903e4f9
Jesse House <jesse.house@codingzeal.com>
2023-09-29 09:56:01 -0700

    add middleware: root redirect and logging

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  console.log(`${request.method}: ${request.nextUrl.pathname}`);
+
+  if (request.nextUrl.pathname === "/") {
+    console.log("GET: / => Redirecting to /home");
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+}
+
+export const config = {
+  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
+};
