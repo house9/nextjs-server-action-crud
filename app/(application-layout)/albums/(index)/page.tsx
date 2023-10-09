a174628df136759736e8e64963f35988f66b6f9d
Jesse House <jesse.house@codingzeal.com>
2023-10-09 15:38:53 -0700

    fix table column header

--- app/(application-layout)/albums/(index)/page.tsx
+++ app/(application-layout)/albums/(index)/page.tsx
@@ -47,7 +47,7 @@ export default async function AlbumIndexPage() {
                       scope="col"
                       className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase"
                     >
-                      Song Name
+                      Album Name
                     </th>
                     <th
                       scope="col"
