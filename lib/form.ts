dbc7e252ac02b526a4f0bc0b2aecb5813ff5e38f
Jesse House <jesse.house@codingzeal.com>
2023-10-12 11:02:14 -0700

    refactor onSubmitConfirm

--- lib/form.ts
+++ lib/form.ts
@@ -9,22 +9,17 @@ type Props = {
  * Returns a function that can be used as an onSubmit handler for a form.
  * When the form is submitted, this function will display a confirmation message to the user.
  *
- * If the user confirms the submission,
- * the function will return true and allow the form to be submitted.
+ * If the user confirms the submission the form will be submitted.
  *
- * If the user cancels the submission,
- * the function will prevent the form from being submitted and return false.
+ * If the user cancels the submission, the function will prevent the form from being submitted.
  *
  * @param message The confirmation message to display to the user.
  * @returns A function that can be used as an onSubmit handler for a form.
  */
 export const onSubmitConfirm = ({ message }: Props) => {
   return (e: FormEvent<HTMLFormElement>) => {
-    if (confirm(message)) {
-      return true;
-    } else {
+    if (!confirm(message)) {
       e.preventDefault();
-      return false;
     }
   };
 };
